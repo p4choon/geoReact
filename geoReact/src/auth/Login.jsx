e85613f6d756e40eef57ec0d84ce420b6ba86c37
hugofq <huflqu@fp.insjoaquimmir.cat>
2023-01-23 19:12:50 +0100

    Parte 3 empezada

--- geoReact/src/auth/Login.jsx
+++ geoReact/src/auth/Login.jsx
@@ -1,15 +1,51 @@
 import './Login.css'
+import { useState } from "react";
+
 
 export default function Login({ setCanvi }) {
+
+  let [name, setName] = useState("");
+  let [password, setPassword] = useState("");
+
+  const sendLogin = (e) => {
+    e.preventDefault();
+
+
+    console.log("Comprovant credencials....");
+    // Enviam dades a l'aPI i recollim resultat
+    fetch("https://backend.insjoaquimmir.cat/api/login", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({ email: email, password: password })
+    })
+      .then((data) => data.json())
+      .then((resposta) => {
+        console.log(resposta);
+        if (resposta.success === true) {
+          alert(resposta.authToken);
+        }
+      })
+      .catch((data) => {
+        console.log(data);
+        alert("Catchch");
+      });
+
+
+    alert("Dades enviades:  " + name + "/" + password);
+  };
 
   return (
     <div class="login">
       <h1>Login</h1>
       <form method="post">
-        <input type="text" name="u" placeholder="Username" required="required" />
-          <input type="password" name="p" placeholder="Password" required="required" />
-          <button type="submit" class="btn btn-primary btn-block btn-large">Login</button>
-          <button onClick={() => {setCanvi(false);}}>Canvia a Register</button>
+        <input type="text" name="name" placeholder="Nombre" required="required" onChange={(e) => {setName(e.target.value)}}/>
+        <input type="password" name="password" placeholder="Password" required="required" onChange={(e) => {setPassword(e.target.value)}}/>
+        <button type="submit" class="btn btn-primary btn-block btn-large" onClick={(e) => {sendLogin(e)}}>Login</button>
+        <br/>
+        <button class="btn btn-primary btn-block btn-large" onClick={() => {setCanvi(false);}}>Canvia a Register</button>
       </form>
     </div>
   )
--- geoReact/src/auth/Register.jsx
+++ geoReact/src/auth/Register.jsx
@@ -1,19 +1,48 @@
 import './Register.css'
+import { useState } from "react";
 
 export default function Register({ setCanvi }) {
+
+  let [formulari, setFormulari] = useState({});
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setFormulari({
+      ...formulari,
+      [e.target.name]: e.target.value
+    });
+  };
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    let { name, password, password2, email } = formulari;
+    alert(
+      "He enviat les Dades:  " +
+        name +
+        "/" +
+        email +
+        "/" +
+        password +
+        "/" +
+        password2
+    );
+  };
+
 
   return (
     <div className="login">
         <h1>Register</h1>
         <form action="/" method="POST" id="register">
-            <input type="text" name="name" placeholder="Nombre"></input><br/>
-            <input type="text" name="surname" placeholder="Apellidos"></input><br/>
-            <input type="email" name="email" placeholder="Correo electr&oacute;nico"></input><br/>
-            <input type="password" name="password" placeholder="Contrase&ntilde;a"></input><br/>
-            <input type="password" name="password2" placeholder="Repite la Contrase&ntilde;a"></input><br/>
-            <button type="submit">Entrar</button>
-            <button type="reset">Limpiar</button>
-            <button onClick={() => {setCanvi(true);}}>Canvia a blanc</button>
+            <input type="text" name="name" placeholder="Nombre" onChange={handleChange}></input><br/>
+            <input type="email" name="email" placeholder="Correo electr&oacute;nico" onChange={handleChange}></input><br/>
+            <input type="password" name="password" placeholder="Contrase&ntilde;a" onChange={handleChange}></input><br/>
+            <input type="password" name="password2" placeholder="Repite la Contrase&ntilde;a" onChange={handleChange}></input><br/>
+            <button class="btn btn-primary btn-block btn-large" onClick={(e) => {handleRegister(e);}}>Register</button>
+            <br></br>
+            <button class="btn btn-primary btn-block btn-large" type="reset">Limpiar</button>
+            <br></br>
+            <button class="btn btn-primary btn-block btn-large" onClick={() => {setCanvi(true);}}>Canvia a login</button>
         </form>
     </div>
   )
